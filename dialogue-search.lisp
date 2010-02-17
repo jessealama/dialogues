6f73ac94005ef9949eae48ba63632e08ba08fd0f
Jesse Alama <alama@stanford.edu>
2010-02-17 15:25:43 +0100

    Search for dialogues.

--- dialogue-search.lisp
+++ dialogue-search.lisp
@@ -1,83 +1,60 @@
-;;; dialogue-search.lisp Systematically explore dialogue games as search trees
+;;; dialogue-search.lisp Dialogue games as search trees
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
-  (require 'dialogues "dialogues.lisp"))
+  (require 'utils "utils.lisp")
+  (require 'dialogues "dialogues.lisp")
+  (require 'search "search.lisp"))
 
-(defun dialogue-successors (dialogue rules)
-  (let (extensions)
-    (dolist (player '(p o) extensions)
-      (dolist (stance '(a d))
-	(let ((next-moves (next-moves dialogue rules player stance)))
-	  (setf extensions
-		(append extensions
-			(mapcar #'(lambda (next-move)
-				    (destructuring-bind (statement index)
-					next-move
-				      (freshly-extend-dialogue dialogue
-							       player
-							       stance
-							       statement
-							       index)))
-				next-moves))))))))
-(defvar *dialogue* nil)
-(defvar *rules* nil)
-(defvar *remainder* nil)
-(defvar *all-done?* nil)
+(defstruct (dialogue-search-problem
+	     (:include problem))
+  (rules nil :type list)
+  (signature nil :type signature))
 
-(defun dialogue-bfs-toplevel (dialogue rules)
-  (cond (*remainder*
-	 (error "The variable *REMAINDER* has a non-null value; don't you want to save the old results?"))
-	(*rules* 
-	 (error "The variable *RULES* has a non-null value -- why?"))
-	(*dialogue*
-	 (error "The variable *DIALOGUE* has a non-null value -- why?"))
-	(t
-	 (setf *all-done?* nil)
-	 (setf *dialogue* dialogue)
-	 (setf *rules* rules)
-	 (dialogue-bfs dialogue rules nil))))
+(defmethod successors ((dsp dialogue-search-problem) node)
+  (let ((dialogue-so-far (node-state node))
+	(rules (dialogue-search-problem-rules dsp)))
+    (let (extensions)
+      (dolist (player '(p o) extensions)
+	(dolist (stance '(a d))
+	  (let ((next-moves (next-moves dialogue-so-far rules player stance)))
+	    (push-all (mapcar #'(lambda (next-move)
+				  (destructuring-bind (statement index)
+				      next-move
+				    (cons (list player stance index statement)
+					  (freshly-extend-dialogue dialogue-so-far
+								   player
+								   stance
+								   statement
+								   index))))
+			    next-moves)
+		    extensions)))))))
 
-(defun d-dialogue-bfs-toplevel (dialogue)
-  (dialogue-bfs-toplevel dialogue d-dialogue-rules))
+(defmethod goal-test ((dsp dialogue-search-problem) node)
+  (let ((successors (successors dsp node)))
+    (setf (node-successors node) successors)
+    (null successors)))
 
-(defun e-dialogue-bfs-toplevel (dialogue)
-  (dialogue-bfs-toplevel dialogue d-dialogue-rules))
+(defun dialogue-search (rules initial-statement signature)
+  (if (formula? initial-statement signature)
+      (let* ((initial-state (make-dialogue initial-statement signature))
+	     (problem (make-dialogue-search-problem :initial-state initial-state
+						    :signature signature
+						    :rules rules)))
+	(breadth-first-search problem))
+      (error "The initial statement ~A is not a formula according to the given signature ~A" initial-statement signature)))
+  
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Searching for strategies
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defun clear-bfs-state ()
-  (setf *dialogue* nil
-	*rules* nil
-	*all-done?* nil
-	*remainder* nil))
+(defstruct (dialogue-strategy-search-problem
+	     (:include problem))
+  (rules nil :type list)
+  (signature nil :type signature))
 
-(defun fresh-dialogue-bfs (dialogue rules)
-  (clear-bfs-state)
-  (dialogue-bfs dialogue rules))
-
-(defun fresh-d-dialogue-bfs (dialogue)
-  (fresh-dialogue-bfs dialogue d-dialogue-rules))
-
-(defun fresh-e-dialogue-bfs (dialogue)
-  (fresh-dialogue-bfs dialogue e-dialogue-rules))
-
-(defun dialogue-bfs (dialogue rules &optional (yet-to-consider nil))
-  (unless *all-done?*
-    (let (current-dialogue solution)
-      (unless yet-to-consider
-	(push dialogue yet-to-consider))
-      (until (or solution (null yet-to-consider))
-	(setf current-dialogue (pop yet-to-consider))
-	(let ((successors (dialogue-successors current-dialogue rules)))
-	  (if successors
-	      (setf yet-to-consider
-		    (append yet-to-consider successors))
-	      (setf solution current-dialogue))))
-      (setf *remainder* yet-to-consider)
-      (unless *remainder*
-	(setf *all-done?* t))
-      solution)))
-
-(defun continue-dialogue-bfs ()
-  (dialogue-bfs *dialogue* *rules* *remainder*))
+(defun strategy-successors (strategy rules)
+  (declare (ignore strategy rules))
+  nil)
 
 (provide 'dialogue-search)
 
