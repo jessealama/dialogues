82c14cf7cb9800a248fd7b2735be46fd9bd74f91
Jesse Alama <alama@stanford.edu>
2010-05-11 22:51:05 +0100

    Export our named formulas.
    Perhaps these should really be in some kind of general logic package.
    But for now, they are where they are.
    
    Am I going to have to eventually export everything?  I haven't really
    thought through the problem of defining an interface for this thing.
    I've just been programming this as though I have access to everything
    all the time.  Now that I'm using ASDF, and defining packages, I have
    to be more sensitive to this issue.  I suppose I could always just
    export every single symbol, but that doesn't feel right to me (though
    even in this case it still would make sense to use ASDF).

--- packages.lisp
+++ packages.lisp
@@ -2,4 +2,20 @@
 (in-package :cl-user)
 
 (defpackage :dialogues
-  (:use :cl))
+  (:use :cl)
+  (:export ; utils
+           "EMPTY-STRING?" ; funny: this has nothing to do with dialogues
+	   ; named formulas
+	   "peirce-formula"
+	   "excluded-middle"
+	   "dummett-formula"
+	   "markov-formula"
+	   "double-negation-intro"
+	   "double-negation-elimination"
+	   "k-formula"
+	   "b-formula"
+	   "c-formula"
+	   "w-formula"
+	   "weak-excluded-middle"
+	   "scott-formula"
+	   "smetanich-formula"))
