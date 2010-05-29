47d1ab0bc3a503ba63ca2b4b7e276af414ec67fd
Jesse Alama <alama@stanford.edu>
2010-05-29 12:05:29 +0100

    Define some new error conditions for when junk is to be added to a signature.
    And define methods for adding functions, constants, and predicates qua strings to a signature (checking for whether the proposed new identifier name is valid).

--- formulas.lisp
+++ formulas.lisp
@@ -20,6 +20,37 @@
 	      :type list
 	      :accessor signature-functions)))
 
+(define-condition unacceptable-identifier-name-error (error)
+  ((text :initarg :text
+	 :reader unacceptable-identifier-name-error-text))
+  (:report (lambda (condition stream)
+	     (let ((text (unacceptable-identifier-name-error-text condition)))
+	       (format stream "The given string,~%~%  ~A,~%~%is an unacceptable as the name of an identifier.")))))	     
+
+(define-condition symbol-already-present-error (error)
+  ((symbol :initarg :text 
+	   :reader symbol-already-present-error-symbol)
+   (signature :initarg :signature 
+	      :reader symbol-already-present-error-signature))
+  (:report (lambda (condition stream)
+	     (let ((symbol (symbol-already-present-error-symbol condition))
+		   (sig (symbol-already-present-error-signature condition)))
+	       (if sig
+		   (if symbol
+		       (format stream 
+			       "The given symbol,~%~%  ~A,~%~%already belongs to the signature~%~%  ~A~%"
+			       symbol sig)
+		       (format stream
+			       "Weird: although a signature,~%~%  ~A~%~%was supplied, no symbol was given (or, more precisely, the given symbol is NIL).  It would seem that this error condition was not properly initialized by the calling code.~%"
+			       sig))
+		   (if symbol
+		       (format stream
+			       "Weird: unable to determine whether the given symbol,~%~%  ~A,~%~%aready belongs to the signature because no signature was supplied.  It woud seem that this error condition was not properly initialized by the calling code.~%"
+			       symbol)
+		       (format stream
+			       "Weird: neither a signature nor a text were supplied. It would seem that this error condition was not properly initialized by the calling code.~%")))))))
+  
+
 (defun copy-signature (signature)
   (make-instance 'signature
 		 :constants (copy-list (signature-constants signature))
@@ -36,6 +67,7 @@
 	      (or functions "(none)")
 	      (or predicates "(none)")))))
 
+(defgeneric belongs-to-signature? (signature thing))
 (defgeneric add-constant (signature new-name))
 (defgeneric delete-constant (signature name))
 (defgeneric add-function (signature new-name new-arity))
@@ -113,50 +145,81 @@
        (not (empty-string? str))
        (not (contains-whitespace? str))))
 
-(defmethod add-constant ((sig signature) constant)
-  (cond ((predicate? sig constant)
-	 (error "The symbol ~A is already reserved as a predicate in the the given signature" constant))
-	((function? sig constant)
-	 (error "The symbol ~A is alredy reserved as a function in the given signature" constant))
-	((constant? sig constant)
-	 (error "The symbol ~A is already a constant in the given signature" constant))
-	(t (push constant (signature-constants sig)))))
+(defmethod belongs-to-signature? ((sig signature) (sym symbol))
+  (or (constant? sig sym) 
+      (predicate? sig sym)
+      (function? sig sym)))
 
-(defmethod delete-constant ((sig signature) constant)
+(defmethod belongs-to-signature? ((sig signature) (str string))
+  (belongs-to-signature? sig (symbolify str)))
+
+(defmethod add-constant ((sig signature) (constant-sym symbol))
+  (cond ((belongs-to-signature? sig constant-sym)
+	 (error 'symbol-already-present-error
+		:symbol constant-sym
+		:signature sig))
+	(t (push constant-sym (signature-constants sig))
+	   sig)))
+
+(defmethod add-constant ((sig signature) (constant-str string))
+  (if (valid-identifier-name? constant-str)
+      (add-constant sig (symbolify constant-str))
+      (error 'unacceptable-identifier-name-error
+	     :text constant-str)))
+
+(defmethod delete-constant ((sig signature) (constant-sym symbol))
   (setf (signature-constants sig)
-	(remove constant (signature-constants sig))))
+	(remove constant-sym (signature-constants sig))))
 
-(defmethod add-predicate ((sig signature) pred-sym arity)
-  (cond ((constant? sig pred-sym)
-	 (error "The symbol ~A is already reserved as a constant in the the given signature" pred-sym))
-	((predicate? sig pred-sym)
-	 (error "The symbol ~A is alredy reserved as a predicate in the given signature" pred-sym))
-	((function? sig pred-sym)
-	 (error "The symbol ~A is alredy reserved as a function in the given signature" pred-sym))
-	((constant? sig pred-sym)
-	 (error "The symbol ~A is already a constant in the given signature" pred-sym))
-	(t (push (cons pred-sym arity) (signature-predicates sig)))))
+(defmethod delete-constant ((sig signature) (constant-str string))
+  (delete-constant sig (symbolify constant-str)))
 
-(defmethod delete-predicate ((sig signature) pred-sym)
+(defmethod add-predicate ((sig signature) (pred-sym symbol) (arity integer))
+  (cond ((belongs-to-signature? sig pred-sym)
+	 (error 'symbol-already-present-error
+		:symbol pred-sym
+		:signature sig))
+	(t (setf (signature-predicates sig)
+		 (acons pred-sym arity (signature-predicates sig)))
+	   sig)))
+
+(defmethod add-predicate ((sig signature) (pred-str string) (arity integer))
+  (if (valid-identifier-name? pred-str)
+      (add-predicate sig (symbolify pred-str) arity)
+      (error 'unacceptable-identifier-name-error
+	     :text pred-str)))
+
+(defmethod delete-predicate ((sig signature) (pred-sym symbol))
   (setf (signature-predicates sig)
 	(remove (find pred-sym (signature-predicates sig) :key #'first)
 		(signature-predicates sig))))
 
-(defmethod add-function ((sig signature) func-sym arity)
-  (cond ((constant? sig func-sym)
-	 (error "The symbol ~A is already reserved as a constant in the the given signature" func-sym))
-	((predicate? sig func-sym)
-	 (error "The symbol ~A is alredy reserved as a predicate in the given signature" func-sym))
-	((function? sig func-sym)
-	 (error "The symbol ~A is alredy reserved as a function in the given signature" func-sym))
-	((constant? sig func-sym)
-	 (error "The symbol ~A is already a constant in the given signature" func-sym))
-	(t (push (cons func-sym arity) (signature-functions sig)))))
+(defmethod delete-predicate ((sig signature) (pred-str string))
+  (delete-predicate sig (symbolify pred-str)))
+
+(defmethod add-function ((sig signature) (func-sym symbol) (arity integer))
+  (cond ((belongs-to-signature? sig func-sym)
+	 (error 'symbol-already-present-error
+		:symbol func-sym
+		:signature sig))
+	(t
+	 (setf (signature-functions sig)
+	       (acons func-sym arity (signature-functions sig)))
+	 sig)))
+
+(defmethod add-function ((sig signature) (func-str string) (arity integer))
+  (if (valid-identifier-name? func-str)
+      (add-function sig (symbolify func-str) arity)
+      (error 'unacceptable-identifier-name-error
+	     :text pred-str)))      
 
 (defmethod delete-function ((sig signature) func-sym)
   (setf (signature-functions sig)
 	(remove (find func-sym (signature-functions sig) :key #'first)
 		(signature-functions sig))))
+
+(defmethod delete-function ((sig signature) (func-str string))
+  (delete-function sig (symbolify func-str)))
 
 (defun read-signature (prompt)
   (let (constants predicates functions)
