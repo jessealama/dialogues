5cefdb4a94778eae7f15bd43df7276848359d11b
Jesse Alama <alama@stanford.edu>
2010-07-06 14:48:48 +0100

    Change header.

--- famous-formulas.lisp
+++ famous-formulas.lisp
@@ -1,65 +1,80 @@
-;;; famous-formulas.lisp Some interesting, famous formulas
+;;; famous-formulas.lisp Some interesting/famous formulas
 
 (in-package :dialogues)
 
-(defconstant-if-unbound peirce-formula 
-  (make-implication (make-implication (make-implication 'p 'q) 'p) 'p))
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (let ((p (make-atomic-formula 'p))
+	(q (make-atomic-formula 'q))
+	(r (make-atomic-formula 'r))
+	(s (make-atomic-formula 's)))
+    
+    (defconstant peirce-formula 
+      (-> (-> (-> p q) p) p))
 
-(defconstant-if-unbound excluded-middle 
-    (make-binary-disjunction 'p (negate 'p)))
+    (defconstant excluded-middle 
+      (v p (¬ p)))
 
-(defconstant-if-unbound dummett-formula 
-    (make-binary-disjunction (make-implication 'p 'q)
-			     (make-implication 'q 'p)))
+    (defconstant dummett-formula 
+      (v (-> p q) (-> q p)))
 
-(defconstant-if-unbound markov-formula 
-    (make-implication (negate (negate 'p)) 
-		      'p))
+    (defconstant double-negation-intro
+      (-> p
+	  (¬ (¬ p))))
+    
+    (defconstant double-negation-elimination
+      (-> (¬ (¬ p)) p))
+    
+    (defconstant k-formula
+      (-> p (-> q p)))
+    
+    (defconstant b-formula
+      (->
+       (-> p q)
+       (->
+	(-> r p)
+	(-> r q))))
 
-(defconstant-if-unbound double-negation-intro
-    (make-implication 'p
-		      (negate (negate 'p))))
+    (defconstant c-formula
+      (->
+       (-> p
+	   (-> q r))
+       (-> q
+	   (-> p r))))
 
-(defconstant-if-unbound double-negation-elimination
-    (make-implication (negate (negate 'p))
-		      'p))
+    (defconstant w-formula 
+      (->
+       (-> p
+	   (-> p q))
+       (-> p q)))
 
-(defconstant-if-unbound k-formula
-    (make-implication 'p
-		      (make-implication 'q 'p)))
+    (defconstant weak-excluded-middle
+      (v
+       (¬ p)
+       (¬ (¬ p))))
+    
+    (defconstant scott-formula 
+      (-> (-> double-negation-elimination excluded-middle)
+	  weak-excluded-middle))
 
-(defconstant-if-unbound b-formula
-    (make-implication
-     (make-implication 'p 'q)
-     (make-implication
-      (make-implication 'r 'p)
-      (make-implication 'r 'q))))
+    (defconstant smetanich-formula 
+      (-> (-> (¬ q) p) peirce-formula))
+    
+    (defconstant de-morgan-not-and-implies-or
+      (-> (¬ (& p q))
+	  (v (¬ p) (¬ q))))
 
-(defconstant-if-unbound c-formula
-    (make-implication
-     (make-implication 'p
-		       (make-implication 'q 'r))
-     (make-implication 'q
-		       (make-implication 'p 'r))))
+    (defconstant de-morgan-not-or-implies-and
+      (-> (¬ (v p q))
+	  (& (¬ p) (¬ q))))
 
-(defconstant-if-unbound w-formula 
-    (make-implication
-     (make-implication 'p
-		       (make-implication 'p 'q))
-     (make-implication 'p 'q)))
+    (defconstant de-morgan-and-not-implies-not-or
+      (-> (& (¬ p) (¬ q))
+	  (¬ (v p q))))
 
-(defconstant-if-unbound weak-excluded-middle
-    (make-binary-disjunction
-     (negate 'p)
-     (negate (negate 'p))))
+    (defconstant de-morgan-or-not-implies-not-and
+      (-> (v (¬ p) (¬ q))
+	  (¬ (& p q))))
 
-(defconstant-if-unbound scott-formula 
-    (make-implication (make-implication double-negation-elimination
-					excluded-middle)
-		      weak-excluded-middle))
-
-(defconstant-if-unbound smetanich-formula 
-    (make-implication (make-implication (negate 'q) 'p)
-		      peirce-formula))
+    ))
 
 ;;; famous-formulas.lisp ends here
