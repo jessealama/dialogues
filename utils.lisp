9b73fae6418399dd890561008d688d35a47f9cc7
Jesse Alama <jesse.alama@gmail.com>
2013-03-22 11:48:41 +0100

    FORMAT magic.

--- utils.lisp
+++ utils.lisp
@@ -137,19 +137,8 @@
 ;;; Input and output
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; This could be done iteratively rather than recursively, but I'm not
-;; sure it makes much of a difference.  I don't expect this function
-;; to get called very much anyway.
 (defun comma-separated-list (lst)
-  (if lst
-      (let ((head (car lst))
-	    (tail (cdr lst)))
-	(concatenate 'string
-		     (if tail
-			 (format nil "~A, " head)
-			 (format nil "~A" head))
-		     (comma-separated-list tail)))
-      ""))
+  (format nil "~{~a~^, ~}" lst))
 
 (defun read-symbol (&rest symbols)
   (let (response)
