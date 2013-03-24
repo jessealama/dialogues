352554fda0b09e085309a5b80117d9da0812f332
Jesse Alama <jesse.alama@gmail.com>
2013-03-24 12:00:19 +0100

    Export more symbols: formula classes, famous formulas, etc.

--- packages.lisp
+++ packages.lisp
@@ -9,6 +9,7 @@
 	   #:PQRS-PROPOSITIONAL-SIGNATURE
 	   #:MAKE-DIALOGUE
 	   #:MOVE
+	   #:term
 	   #:MAKE-MOVE
 	   #:DIALOGUE-PLAYS
 	   #:EXTEND-DIALOGUE
@@ -27,8 +28,31 @@
 
 	   ; rule sets
 	   #:D-DIALOGUE-RULES
+	   #:e-dialogue-rules
+	   #:classical-dialogue-rules
+	   #:nearly-classical-dialogue-rules
+	   #:d-dialogue-rules-queue
+	   #:e-dialogue-rules-queue
+	   #:conjectural-classical-dialogue-rules
+	   #:d-dialogue-rules-minus-d10
+	   #:d-dialogue-rules-minus-d11
+	   #:d-dialogue-rules-minus-d12
+	   #:e-dialogue-rules-minus-d11
+	   #:e-dialogue-rules-minus-d12
+	   #:d-dialogue-rules-symmetric-d13
+	   #:d-dialogue-rules-literal-d10
+	   #:e-dialogue-rules-literal-d10
+	   #:only-particle-rules
+	   #:particle-rules+d10
+	   #:particle-rules+d11
+	   #:particle-rules+d12
+	   #:particle-rules+d13
+	   #:particle-rules+e
+	   #:sara-ad-hoc-rules
+	   #:sara-ad-hoc-rules-2
 
 	   ;; slots
+	   #:description
 
 	   ; move class
 	   #:PLAYER
@@ -40,6 +64,21 @@
 	   #:SIGNATURE
 	   #:PLAYS
 	   #:DIALOGUE-SIGNATURE
+
+	   ;; formulas classes
+	   #:unary-connective-formula
+	   #:binary-connective-formula
+	   #:binary-conjunction
+	   #:binary-disjunction
+	   #:implication
+	   #:equivalence
+	   #:negation
+	   #:generalization
+	   #:universal-generalization
+	   #:existential-generalization
+	   #:atomic-formula
+
+	   #:lex<
 
 	   ; named formulas
 	   #:PEIRCE-FORMULA
@@ -79,6 +118,92 @@
 	   #:identity-translation
 	   #:atomic->excluded-middle-translation
 	   #:converse-translation
+
+	   ;; formulas
+	   #:peirce-formula
+	   #:excluded-middle
+	   #:weak-excluded-middle
+	   #:conditional-excluded-middle
+	   #:dummett-formula
+	   #:double-negation-introduction
+	   #:double-negation-elimination
+	   #:k-formula
+	   #:b-formula
+	   #:c-formula
+	   #:w-formula
+	   #:i-formula
+	   #:s-formula
+	   #:scott-formula
+	   #:smetanich-formula
+	   #:de-morgan-not-and-implies-or-not
+	   #:de-morgan-not-or-implies-and-not
+	   #:de-morgan-and-not-implies-not-or
+	   #:de-morgan-or-not-implies-not-and
+	   #:anti-connexive-formula
+	   #:ex-contradictione-quodlibet
+	   #:implicational-ex-falso
+	   #:kp
+	   #:wkp
+	   #:distributivity-of-implication-over-disjunction
+	   #:aristotles-thesis-positive-antecedent
+	   #:aristotles-thesis-negative-antecedent
+	   #:modus-ponens
+	   #:modus-tollens
+	   #:hypothetical-syllogism
+	   #:disjunctive-syllogism
+	   #:constructive-dilemma
+	   #:destructive-dilemma
+	   #:conjunction-elimination-left
+	   #:conjunction-elimination-right
+	   #:conjunction-introduction
+	   #:conjunction-introduction
+	   #:disjunction-introduction-left
+	   #:disjunction-introduction-right
+	   #:composition
+	   #:commutivity-of-conjunction
+	   #:commutativity-of-disjunction
+	   #:commutativity-of-implication
+	   #:associativity-of-conjunction
+	   #:associativity-of-disjunction
+	   #:associativity-of-implication
+	   #:distributivity-of-conjunction-over-disjunction-conjunctive-antecedent
+	   #:distributivity-of-conjunction-over-disjunction-disjunctive-antecedent
+	   #:distributivity-of-disjunction-over-conjunction-disjunctive-antecedent
+	   #:distributivity-of-disjunction-over-conjunction-conjunctive-antecedent
+	   #:transposition
+	   #:material-implication-implicational-antecedent
+	   #:material-implication-disjunctive-antecedent
+	   #:material-implication-negative-antecedent
+	   #:material-implication-negative-consequent
+	   #:material-equivalence-conjunctive-antecedent
+	   #:material-equivalence-disjunctive-antecedent
+	   #:exportation-conjunctive-antecedent
+	   #:exportation-implicational-antecedent
+	   #:conjunctive-idempotency-conjunctive-antecedent
+	   #:conjunctive-idempotency-conjunctive-consequent
+	   #:disjunctive-idempotenency-disjunctive-antecedent
+	   #:disjunctive-idempotenency-disjunctive-consequent
+	   #:disjunctive-absorption-disjunctive-antecedent
+	   #:disjunctive-absorption-disjunctive-consequent
+	   #:conjunctive-absorption-conjunctive-antecedent
+	   #:conjunctive-absorption-conjunctive-consequent
+	   #:frege-formula
+	   #:contraposition-positive-antecedent
+	   #:contraposition-negative-antecedent
+	   #:connexive-ax-1
+	   #:connexive-ax-2
+	   #:connexive-ax-3
+	   #:connexive-ax-4
+	   #:connexive-ax-5
+	   #:connexive-ax-6
+	   #:connexive-ax-7
+	   #:connexive-ax-8
+	   #:connexive-ax-9
+	   #:connexive-ax-10
+	   #:connexive-ax-11
+	   #:connexive-ax-12
+	   #:il-disjunction-formula
+	   #:il-negation-formula
 
 	   ;; server configuration
 	   #:*DIALOGUE-SERVER-PORT*))
