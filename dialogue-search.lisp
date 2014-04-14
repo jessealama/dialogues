288a9b5bc1e72db54de460ec3790faa71c79bdad
Jesse Alama <jesse.alama@gmail.com>
2014-04-14 14:46:38 +0200

    Dump signature.

--- dialogue-search.lisp
+++ dialogue-search.lisp
@@ -4,8 +4,7 @@
 
 (defstruct (dialogue-search-problem
 	     (:include problem))
-  (rules nil :type (or (eql nil) ruleset))
-  (signature nil :type (or (eql nil) signature)))
+  (rules nil :type (or (eql nil) ruleset)))
 
 (defmethod goal-test ((problem dialogue-search-problem) node)
   (expand node problem)
@@ -30,51 +29,35 @@
 			    next-moves)
 		    extensions))))))
 
-(defun dialogue-search-bfs (rules initial-statement signature &optional more-nodes)
-  (if (belongs-to-signature? signature initial-statement)
-      (let* ((initial-state (make-dialogue initial-statement signature rules))
-	     (problem (make-dialogue-search-problem :initial-state initial-state
-						    :signature signature
-						    :rules rules)))
-	(breadth-first-search-for-bottom-with-nodes problem more-nodes))
-      (error "The initial statement ~A is not a formula according to the given signature ~A" initial-statement signature)))
+(defun dialogue-search-bfs (rules initial-statement &optional more-nodes)
+  (let* ((initial-state (make-dialogue initial-statement rules))
+         (problem (make-dialogue-search-problem :initial-state initial-state
+                                                :rules rules)))
+    (breadth-first-search-for-bottom-with-nodes problem more-nodes)))
 
-(defun dialogue-search-dfs (rules initial-statement signature)
-  (if (belongs-to-signature? signature initial-statement)
-      (let* ((initial-state (make-dialogue initial-statement signature rules))
-	     (problem (make-dialogue-search-problem :initial-state initial-state
-						    :signature signature
-						    :rules rules)))
-	(depth-first-search-for-bottom problem))
-      (error "The initial statement ~A is not a formula according to the given signature ~A" initial-statement signature)))
+(defun dialogue-search-dfs (rules initial-statement)
+  (let* ((initial-state (make-dialogue initial-statement rules))
+         (problem (make-dialogue-search-problem :initial-state initial-state
+                                                :rules rules)))
+    (depth-first-search-for-bottom problem)))
 
-(defun dialogue-search-dfs-no-cycles (rules initial-statement signature)
-  (if (belongs-to-signature? signature initial-statement)
-      (let* ((initial-state (make-dialogue initial-statement signature rules))
-	     (problem (make-dialogue-search-problem :initial-state initial-state
-						    :signature signature
-						    :rules rules)))
-	(no-cycles-depth-first-search-for-bottom problem #'equal-dialogues?))
-      (error "The initial statement ~A is not a formula according to the given signature ~A" initial-statement signature)))
+(defun dialogue-search-dfs-no-cycles (rules initial-statement)
+  (let* ((initial-state (make-dialogue initial-statement rules))
+         (problem (make-dialogue-search-problem :initial-state initial-state
+                                                :rules rules)))
+    (no-cycles-depth-first-search-for-bottom problem #'equal-dialogues?)))
 
-(defun bounded-dialogue-search-dfs (rules initial-statement signature depth &optional (initial-state (make-dialogue initial-statement signature rules)))
-  (if (belongs-to-signature? signature initial-statement)
-      (let ((problem (make-dialogue-search-problem :initial-state initial-state
-						   :signature signature
-						   :rules rules)))
-	(depth-limited-dfs-search problem depth))
-      (error "The initial statement ~A is not a formula according to the given signature ~A" initial-statement signature)))
+(defun bounded-dialogue-search-dfs (rules initial-statement depth &optional (initial-state (make-dialogue initial-statement rules)))
+  (let ((problem (make-dialogue-search-problem :initial-state initial-state
+                                               :rules rules)))
+    (depth-limited-dfs-search problem depth)))
 
-(defun bounded-dialogue-search-bfs (rules initial-statement signature depth
-				    &optional (initial-state (make-dialogue initial-statement signature rules))
+(defun bounded-dialogue-search-bfs (rules initial-statement depth
+				    &optional (initial-state (make-dialogue initial-statement rules))
 				              initial-queue)
-
-  (if (belongs-to-signature? signature initial-statement)
-      (let ((problem (make-dialogue-search-problem :initial-state initial-state
-						   :signature signature
+(let ((problem (make-dialogue-search-problem :initial-state initial-state
 						   :rules rules)))
-	(bounded-breadth-first-search-with-nodes problem depth initial-queue))
-      (error "The initial statement ~A is not a formula according to the given signature ~A" initial-statement signature)))
+	(bounded-breadth-first-search-with-nodes problem depth initial-queue)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Searching for strategies
@@ -82,8 +65,7 @@
 
 (defstruct (dialogue-strategy-search-problem
 	     (:include problem))
-  (rules nil :type list)
-  (signature nil :type signature))
+  (rules nil :type list))
 
 (defun strategy-successors (strategy rules)
   (declare (ignore strategy rules))
@@ -185,22 +167,21 @@
 		 :cutoff
 		 every-result)))))
 
-(defun dialogue-valid? (rules signature statement depth)
+(defun dialogue-valid? (rules statement depth)
   "Determine whether statement is valid in the dialogue games defined by
 RULES.  \"Valid\" means that Proponent has a winning strategy in the
 game tree developed down to depth DEPTH."
-  (let ((dialogue (make-dialogue statement signature rules)))
+  (let ((dialogue (make-dialogue statement rules)))
     (if (fast-eval-entire-dialogue dialogue)
 	(proponent-has-winning-strategy? dialogue depth 1)
 	(error 'inappropriate-initial-statement-error
 	       :rules rules
 	       :statement statement))))
 
-(defun classify-formulas (formulas rules signature search-depth)
+(defun classify-formulas (formulas rules search-depth)
   (let (inappropriate valid invalid cutoff)
     (dolist (formula formulas)
       (handler-case (let ((result (dialogue-valid? rules
-						   signature
 						   formula
 						   search-depth)))
 		      (ecase result
@@ -221,8 +202,8 @@ game tree developed down to depth DEPTH."
 
 ;;; Strategy search for specific rulesets
 
-(defun intuitionistically-valid--e? (formula depth signature)
-  (let ((dialogue (make-dialogue formula signature e-dialogue-rules)))
+(defun intuitionistically-valid--e? (formula depth)
+  (let ((dialogue (make-dialogue formula e-dialogue-rules)))
     (if (and (implication? formula)
              (binary-conjunction? (antecedent formula)))
         (let ((a (antecedent formula))
@@ -247,8 +228,8 @@ game tree developed down to depth DEPTH."
             (proponent-has-winning-strategy? dialogue depth 7)))
         (proponent-has-winning-strategy? dialogue depth 1))))
 
-(defun intuitionistically-valid--e-no-pro-repeats? (formula depth signature)
-(let ((dialogue (make-dialogue formula signature e-dialogue-rules-no-pro-repetitions)))
+(defun intuitionistically-valid--e-no-pro-repeats? (formula depth)
+(let ((dialogue (make-dialogue formula e-dialogue-rules-no-pro-repetitions)))
     (if (and (implication? formula)
              (binary-conjunction? (antecedent formula)))
         (let ((a (antecedent formula))
@@ -273,8 +254,8 @@ game tree developed down to depth DEPTH."
             (proponent-has-winning-strategy? dialogue depth 7)))
         (proponent-has-winning-strategy? dialogue depth 1))))
 
-(defun intuitionistically-valid--e-no-repeats? (formula depth signature)
-  (let ((dialogue (make-dialogue formula signature e-dialogue-rules-no-repetitions)))
+(defun intuitionistically-valid--e-no-repeats? (formula depth)
+  (let ((dialogue (make-dialogue formula e-dialogue-rules-no-repetitions)))
     (if (and (implication? formula)
              (binary-conjunction? (antecedent formula)))
         (let ((a (antecedent formula))
@@ -299,22 +280,20 @@ game tree developed down to depth DEPTH."
             (proponent-has-winning-strategy? dialogue depth 7)))
         (proponent-has-winning-strategy? dialogue depth 1))))
 
-(defun intuitionistically-valid--e-no-pro-double-repeats? (formula depth signature)
+(defun intuitionistically-valid--e-no-pro-double-repeats? (formula depth)
   (dialogue-valid? e-dialogue-rules-no-pro-double-repetitions
-                   signature
                    formula
                    depth))
 
-(defun intuitionistically-valid--d? (formula depth signature)
+(defun intuitionistically-valid--d? (formula depth)
   (dialogue-valid? d-dialogue-rules
-                   signature
                    formula
                    depth))
 
 ;; constrained search (not just constrained rulesets)
 
-(defun intuitionistically-valid--e-no-repeats--prefer-defenses? (formula depth signature)
-  (let ((dialogue (make-dialogue formula signature e-dialogue-rules-no-repetitions)))
+(defun intuitionistically-valid--e-no-repeats--prefer-defenses? (formula depth)
+  (let ((dialogue (make-dialogue formula e-dialogue-rules-no-repetitions)))
     (proponent-has-winning-strategy--defenses-preferred? dialogue depth 1)))
 
 (defvar search-tree-directory (make-hash-table :test #'equal)
@@ -360,9 +339,9 @@ DEPTH TREE).")
 (defun dialogue-search-tree (formula rules depth)
   (let ((earlier-entries (search-trees-for-formula-with-rules formula rules))
 	(problem (make-dialogue-search-problem :rules rules
-						   :initial-state (make-dialogue formula
-										 *alphabetic-propositional-signature*
-										 rules))))
+                                               :initial-state (make-dialogue formula
+
+                                                                             rules))))
     (let ((tree (loop
 		   for entry in earlier-entries
 		   do
@@ -472,7 +451,6 @@ is assumed that OPPONENT-NODE is expanded."
   (let* ((problem (make-dialogue-search-problem :rules ruleset
 						:initial-state (or dialogue
 								   (make-dialogue formula
-										  *alphabetic-propositional-signature*
 										  ruleset))))
 	 (root (develop-dialogue-tree-to-depth (create-start-node problem) depth problem)))
     (proponent-ws-from-proponent-node root ruleset)))
