0a52e1bb10bbab43ac314f0b0b9f14133168cb6c
Jesse Alama <jesse.alama@gmail.com>
2014-04-16 12:04:46 +0200

    Factor out generating attacks and defenses.

--- ruleset.lisp
+++ ruleset.lisp
@@ -61,97 +61,106 @@
 (defmethod possible-propositional-attacks ((n negation))
   (list (unnegate n)))
 
+(defun d-possible-attacks (dialogue)
+  (if (empty-dialogue-p dialogue)
+      (mapcar #'(lambda (attack)
+                  (make-instance 'opponent-move
+                                 :statement attack
+                                 :reference 0
+                                 :attack t))
+              (possible-propositional-attacks (initial-formula dialogue)))
+      (let* ((responses nil)
+             (last-move (last-move dialogue))
+             (other-class (if (opponent-move-p last-move)
+                              'proponent-move
+                              'opponent-move))
+             (start (if (opponent-move-p last-move) 1 2)))
+        (loop
+           :with l = (dialogue-length dialogue)
+           :for i :from start :upto l :by 2
+           :for move = (nth-move dialogue i)
+           :for statement = (statement move)
+           :when (and (formula-p statement)
+                      (not (atomic-formula-p statement)))
+           :do
+           (cond ((implication-p statement)
+                  (push (make-instance other-class
+                                       :attack t
+                                       :reference i
+                                       :statement (antecedent statement))
+                        responses))
+                 ((binary-conjunction-p statement)
+                  (push (make-instance other-class
+                                       :attack t
+                                       :reference i
+                                       :statement *attack-left-conjunct*)
+                        responses)
+                  (push (make-instance other-class
+                                       :attack t
+                                       :reference i
+                                       :statement *attack-right-conjunct*)
+                        responses))
+                 (t
+                  (error "How to attack~%~%  ~a~%~%?~%" statement))))
+        ;; ensure that the atom restriction on Proponent is observed
+        (when (opponent-move-p last-move)
+          (setf responses
+                (remove-if #'(lambda (move)
+                               (and (atomic-formula-p move)
+                                    (not (opponent-asserted-atom-earlier? dialogue move))))
+                           responses)))
+        responses)))
+
+(defun d-possible-defenses (dialogue)
+  (unless (empty-dialogue-p dialogue)
+    (let* ((responses nil)
+           (last-move (last-move dialogue))
+           (most-recent (most-recent-open-attack dialogue))
+           (other-class (if (opponent-move-p last-move)
+                            'proponent-move
+                            'opponent-move)))
+      (when (integerp most-recent)
+        (let* ((attack (nth-move dialogue most-recent))
+               (attack-reference (reference attack))
+               (attack-statement (statement attack))
+               (attacked-statement (nth-statement dialogue attack-reference)))
+          (let ((defense (cond ((implication-p attacked-statement)
+                                (consequent attacked-statement))
+                               ((binary-conjunction-p attacked-statement)
+                                (cond ((eql attack-statement *attack-left-conjunct*)
+                                       (lhs attacked-statement))
+                                      ((eql attack-statement *attack-right-conjunct*)
+                                       (rhs attack-statement))
+                                      (t
+                                       (error "The statement~%~%  ~a~%~%was attacked by~%~%  ~a~%~%which is neither ~a nor ~a as we expect." attacked-statement attack-statement *attack-left-conjunct* *attack-right-conjunct*))))
+                               (t
+                                (error "How to defend~%~%  ~a~%~%?~%" attacked-statement)))))
+            (push (make-instance other-class
+                                 :reference most-recent
+                                 :statement defense
+                                 :attack nil)
+                  responses))))
+        ;; filter out duplicate Opponent attacks
+        (when (proponent-move-p last-move)
+          (setf responses
+                (remove-if-not #'(lambda (move)
+                                   (duplicate-opponent-attack? dialogue move))
+                               responses)))
+        ;; ensure that the atom restriction on Proponent is observed
+        (when (opponent-move-p last-move)
+          (setf responses
+                (remove-if #'(lambda (move)
+                               (and (atomic-formula-p move)
+                                    (not (opponent-asserted-atom-earlier? dialogue move))))
+                           responses)))
+        responses)))
+
 (defun d-propositional-expander (dialogue)
   ;; Rules: (1) an attack may be defended only once; (2) P may not
   ;; assert an atom before O; (3) only the most recent open attack may
   ;; be defended.
-  (let ((initial (initial-formula dialogue))
-        (plays (plays dialogue))
-        (last-move (last-move dialogue))
-        (responses nil))
-    (cond ((null plays)
-           (let ((attacks (possible-propositional-attacks initial)))
-             (dolist (attack attacks)
-               (push (make-instance 'opponent-move
-                                    :statement attack
-                                    :reference 0
-                                    :attack t)
-                 responses))))
-          (t
-           ;; Generate all possible defenses.
-           (let ((most-recent (most-recent-open-attack dialogue))
-                 (other-class (if (opponent-move-p last-move)
-                                  'proponent-move
-                                  'opponent-move)))
-             (when (integerp most-recent)
-               (let* ((attack (nth-move dialogue most-recent))
-                      (attack-reference (reference attack))
-                      (attack-statement (statement attack))
-                      (attacked-statement (nth-statement dialogue attack-reference)))
-                 (let ((defense (cond ((implication-p attacked-statement)
-                                       (consequent attacked-statement))
-                                      ((binary-conjunction-p attacked-statement)
-                                       (cond ((eql attack-statement *attack-left-conjunct*)
-                                              (lhs attacked-statement))
-                                             ((eql attack-statement *attack-right-conjunct*)
-                                              (rhs attack-statement))
-                                             (t
-                                              (error "The statement~%~%  ~a~%~%was attacked by~%~%  ~a~%~%which is neither ~a nor ~a as we expect." attacked-statement attack-statement *attack-left-conjunct* *attack-right-conjunct*))))
-
-                                      (t
-                                       (error "How to defend~%~%  ~a~%~%?~%" attacked-statement)))))
-                   (push (make-instance other-class
-                                        :reference most-recent
-                                        :statement defense
-                                        :attack nil)
-                         responses)))))
-           ;; all possible attacks
-           (let ((other-class (if (opponent-move-p last-move)
-                                  'proponent-move
-                                  'opponent-move))
-                 (start (if (opponent-move-p last-move) 1 2)))
-             (loop
-                :with l = (dialogue-length dialogue)
-                :for i :from start :upto l :by 2
-                :for move = (nth-move dialogue i)
-                :for statement = (statement move)
-                :when (and (formula-p statement)
-                           (not (atomic-formula-p statement)))
-                :do
-                (cond ((implication-p statement)
-                       (push (make-instance other-class
-                                            :attack t
-                                            :reference i
-                                            :statement (antecedent statement))
-                             responses))
-                      ((binary-conjunction-p statement)
-                       (push (make-instance other-class
-                                            :attack t
-                                            :reference i
-                                            :statement *attack-left-conjunct*)
-                             responses)
-                       (push (make-instance other-class
-                                            :attack t
-                                            :reference i
-                                            :statement *attack-right-conjunct*)
-                             responses))
-
-                      (t
-                       (error "How to attack~%~%  ~a~%~%?~%" statement)))))
-           ;; filter out illegal assertions by P of atoms
-           (when (opponent-move-p last-move)
-             (setf responses
-                   (remove-if #'(lambda (move)
-                                  (and (atomic-formula-p move)
-                                       (not (opponent-asserted-atom-earlier? dialogue move))))
-                                  responses)))
-           ;; filter out duplicate Opponent attacks
-           (when (proponent-move-p last-move)
-             (setf responses
-                   (remove-if-not #'(lambda (move)
-                                      (duplicate-opponent-attack? dialogue move))
-                                  responses)))))
-    responses))
+  (append (d-possible-defenses dialogue)
+          (d-possible-attacks dialogue)))
 
 (defun e-propositional-expander (dialogue)
   ;; Rules: E rules, with the restriction that Opponent must respond immediately to Proponent.
