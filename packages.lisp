c40237e3ef0a166324e857ccc1e6103875a8d657
Jesse Alama <jesse.alama@gmail.com>
2014-07-10 14:16:07 +0200

    Make the UCW package here.

--- packages.lisp
+++ packages.lisp
@@ -9,6 +9,9 @@
 		#:directory-pathname-p
 		#:pathname-as-directory))
 
+(defpackage :dialogues-ucw
+  (:use :cl :ucw :ucw-core :cl-ppcre :alexandria :dialogues))
+
 (defvar *dialogue-package* (find-package :dialogues))
 
 ;;; packages.lisp ends here
